ca0205e4b743e07d1863adb839bafd8166b4ce7e
Ubuntu <ubuntu@ip-172-31-6-17.eu-west-1.compute.internal>
2016-05-21 22:18:13 +0000

    Pull newer build

--- bootstrap_instance.sh
+++ bootstrap_instance.sh
@@ -5,7 +5,9 @@ HOME=/home/ubuntu
 # install docker
 cd $HOME
 curl -sSL https://get.docker.com/ | sh
-curl https://gitlab.com/itxtech/genisys/builds/1452305/artifacts/file/Genisys_1.1dev-a685f20.phar -o genisys.phar
+
+# get phar
+curl https://gitlab.com/itxtech/genisys/builds/1461919/artifacts/file/Genisys_1.1dev-93aea9c.phar -o genisys.phar
 
 
 # compile working git version
