09551a69fa2402dc35475a5b3038e50ccf681e62
root <root@ip-172-31-6-4.us-west-2.compute.internal>
2016-05-23 20:52:39 +0000

    Added phone home script

--- phone_home.py
+++ phone_home.py
@@ -1,7 +1,14 @@
 import requests
+import datetime
 
-r = requests.get('http://ec2-52-30-111-108.eu-west-1.compute.amazonaws.com:5000/server_data?instance_id=i-31d90dec')
+instance_id = requests.get('http://169.254.169.254/latest/meta-data/instance-id').text
+print instance_id
 
-expiry_date = datetime.datetime.strptime(r['expiry_date'], "%Y-%m-%d %H:%M:%S.%f")
+r = requests.get('http://ec2-52-30-111-108.eu-west-1.compute.amazonaws.com:5000/server_data?instance_id='+instance_id)
 
-print
+expiry_date = datetime.datetime.strptime(r.json()['expiry_date'], "%Y-%m-%d %H:%M:%S.%f")
+
+td = expiry_date - datetime.datetime.now()
+
+print(td.seconds // 3600)
+
