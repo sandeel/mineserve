b31c5ff2bf4e10742a540c5375b5f70baa5411a4
Ubuntu <ubuntu@ip-172-31-6-17.eu-west-1.compute.internal>
2016-05-26 07:04:26 +0000

    Added Promo codes

--- website.py
+++ website.py
@@ -10,6 +10,12 @@ from flask import request
 from flask import redirect
 import stripe
 import os
+import flask_admin as admin
+from flask_admin.contrib import sqla
+from flask_admin.contrib.sqla import filters
+import bcrypt
+import random
+import string
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
@@ -32,11 +38,29 @@ login_manager = flask_login.LoginManager()
 
 login_manager.init_app(app)
 
+class PromoCode(db.Model):
+    code = db.Column(db.String, primary_key=True)
+    activated = db.Column(db.Boolean)
+    reward_code = db.Column(db.String)
+    
+    def __init__(self, reward_code):
+        self.code = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(6))
+        self.activated = False
+        self.reward_code = reward_code
+
+# Customized promo code admin
+class PromoCodeAdmin(sqla.ModelView):
+    column_display_pk=True
+
+    form_choices = { 'reward_code': [ ('0', 'BetaTest'),],}
+
+
 class Server(db.Model):
 
     __tablename__ = 'server'
     id = db.Column(db.String, primary_key=True)
     instance_id = db.Column(db.String)
+    hashed_server_key = db.Column(db.String)
     op = db.Column(db.String)
     expiry_date = db.Column(db.DateTime)
     creation_date = db.Column(db.DateTime)
@@ -53,6 +77,19 @@ class Server(db.Model):
         now = datetime.datetime.now()
         now_plus_5_hours = now + datetime.timedelta(minutes=301)
         self.expiry_date = now_plus_5_hours
+
+        # generate a random key to give the customer and store the hash
+        self.server_key = str(uuid.uuid4())
+        self.hashed_server_key = bcrypt.hashpw(self.server_key, bcrypt.gensalt())
+
+    def __str__(self):
+        return self.id
+
+    def check_server_key(self, server_key):
+        if bcrypt.hashpw(server_key, hashed) == self.hashed_server_key:
+            return True
+        else:
+            return False
 
     @property
     def ip_address(self):
@@ -121,22 +158,11 @@ def server_data():
 def landing_page():
     return render_template('landing_page.html')
 
-@app.route("/create-server", methods=["GET","POST"])
-def create_server():
-    if request.method == "POST":
-        new_server = Server(op=request.form['minecraft_name'])
-        db.session.add(new_server)
-        db.session.commit()
-
-        return redirect("/server/"+new_server.id)
-    return render_template('create_server.html')
-
 @app.route("/server/<server_id>", methods=["GET","POST"])
 def server(server_id):
     server = Server.query.filter_by(id=server_id).first()
     message = """
     """
-
     topped_up_message = None
 
     if (datetime.datetime.now() - server.creation_date).seconds < 3600:
@@ -164,11 +190,17 @@ def server(server_id):
         server.expiry_date = server.expiry_date + datetime.timedelta(days=30)
         db.session.add(server)
         db.session.commit()
+        new_server_key= None
 
         topped_up_message = "Topped up by 30 days."
 
     td = server.expiry_date - datetime.datetime.now()
     days, hours, minutes = td.days, td.seconds // 3600, td.seconds // 60 % 60
+
+    if not request.args:
+        server_key = None
+    else:
+        server_key = request.args['server_key']
 
     return render_template('server.html',
             time_remaining=str(days)+' days, '+str(hours)+' hours, '+str(minutes)+' minutes',
@@ -178,7 +210,27 @@ def server(server_id):
             status=server.status,
             new_server_message=new_server_message,
             topped_up_message=topped_up_message,
+            server_key=server_key,
             )
+
+@app.route("/create-server", methods=["GET","POST"])
+def create_server():
+    if request.method == "POST":
+        new_server = Server(op=request.form['minecraft_name'])
+        db.session.add(new_server)
+        db.session.commit()
+
+
+        return redirect('/server/'+new_server.id+'?server_key='+new_server.server_key)
+    return render_template('create_server.html')
+
+
+
+# Create admin
+admin = admin.Admin(app, name='Example: SQLAlchemy', template_mode='bootstrap3')
+admin.add_view(sqla.ModelView(Server,db.session))
+admin.add_view(PromoCodeAdmin(PromoCode,db.session))
+
 
 if __name__ == '__main__':
     app.run("0.0.0.0", debug=True)
