01b65c2d1abe5e76a514399fbe8e9a63f79249a9
Ubuntu <ubuntu@example.com>
2016-06-05 09:23:50 +0000

    Change logs path

--- application.py
+++ application.py
@@ -35,7 +35,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Handler 
-LOG_FILE = '/var/log/flask.log'
+LOG_FILE = '/opt/python/log/flask.log'
 handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
 handler.setLevel(logging.DEBUG)
 
