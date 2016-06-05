889adcafb1ef31c223a2eb039566a86567e3fe76
Ubuntu <ubuntu@example.com>
2016-06-05 09:48:36 +0000

    Perms for log file

--- application.py
+++ application.py
@@ -35,7 +35,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Handler 
-LOG_FILE = '/home/ec2-user/flask.log'
+LOG_FILE = '/opt/python/log/flask.log'
 handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
 handler.setLevel(logging.DEBUG)
 
