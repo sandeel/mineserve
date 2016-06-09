a0cd84a9a97cab0efe9ad5a3ef2e197ad5a458c2
Ubuntu <ubuntu@example.com>
2016-06-09 06:36:06 +0000

    Disable async

--- bootstrap_instance.sh
+++ bootstrap_instance.sh
@@ -65,7 +65,7 @@ echo Ops are $OPS
 echo $OPS > /home/ubuntu/ops.txt
 
 # start or run container
-docker run -itd --name atlas -p 19132:19132 -p 33775:33775/udp -v /home/ubuntu/plugins:/srv/genisys/plugins -v /home/ubuntu/ops.txt:/srv/genisys/ops.txt -v /home/ubuntu/genisys.phar:/srv/genisys/genisys.phar -v /home/ubuntu/server.properties:/srv/genisys/server.properties -v /home/ubuntu/pocketmine.yml:/srv/genisys/pocketmine.yml --restart=unless-stopped itxtech/docker-env-genisys || docker start atlas
+docker run -itd --name atlas -p 19132:19132 -p 33775:33775/udp -v /home/ubuntu/genisys.yml:/srv/genisys/genisys.yml -v /home/ubuntu/plugins:/srv/genisys/plugins -v /home/ubuntu/ops.txt:/srv/genisys/ops.txt -v /home/ubuntu/genisys.phar:/srv/genisys/genisys.phar -v /home/ubuntu/server.properties:/srv/genisys/server.properties -v /home/ubuntu/pocketmine.yml:/srv/genisys/pocketmine.yml --restart=unless-stopped itxtech/docker-env-genisys || docker start atlas
 
 # set up cron to phone home
 pip install requests
