5c1fd0d45283abc355b9265cd112b231dfc8d75b
Ubuntu <ubuntu@example.com>
2016-06-16 06:19:58 +0000

    Allow insecure curl

--- bootstrap_instance.sh
+++ bootstrap_instance.sh
@@ -17,7 +17,7 @@ pip install awscli
 apt-get -y install unzip
 cd /tmp
 rm -Rf /tmp/mineserve-master
-curl -L https://github.com/sandeel/mineserve/archive/master.zip  -o /tmp/master.zip
+curl -kL https://github.com/sandeel/mineserve/archive/master.zip  -o /tmp/master.zip
 unzip master.zip
 
 
@@ -41,22 +41,22 @@ cp -n /tmp/mineserve-master/resources/server.properties /home/ubuntu/server.prop
 cp -n /tmp/mineserve-master/resources/pocketmine.yml /home/ubuntu/pocketmine.yml
 
 #get server properties
-read INSTANCE_ID <<< $(curl 'http://169.254.169.254/latest/meta-data/instance-id')
+read INSTANCE_ID <<< $(curl -k 'http://169.254.169.254/latest/meta-data/instance-id')
 echo Instance ID is $INSTANCE_ID
 read PHONE_HOME_ENDPOINT <<< $(cat /home/ubuntu/config.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["phone_home_endpoint"]')
 echo Phone home endpoint is $PHONE_HOME_ENDPOINT
-read SERVER_ID <<< $(curl -s ${PHONE_HOME_ENDPOINT}/server_data?instance_id=${INSTANCE_ID} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["id"]')
-read PLUGINS <<< $(curl -s ${PHONE_HOME_ENDPOINT}/server_data?instance_id=${INSTANCE_ID} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["enabled_plugins"]')
+read SERVER_ID <<< $(curl -k -s ${PHONE_HOME_ENDPOINT}/server_data?instance_id=${INSTANCE_ID} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["id"]')
+read PLUGINS <<< $(curl -k -s ${PHONE_HOME_ENDPOINT}/server_data?instance_id=${INSTANCE_ID} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["enabled_plugins"]')
 
 
 echo Server ID is $SERVER_ID
 
 cd $HOME
-curl ${PHONE_HOME_ENDPOINT}/server/${SERVER_ID}/properties -o server.properties
+curl -k ${PHONE_HOME_ENDPOINT}/server/${SERVER_ID}/properties -o server.properties
 cp /home/ubuntu/server.properties /home/ubuntu/server.properties.bk
 
 #op the user
-read OPS <<< $(curl -s ${PHONE_HOME_ENDPOINT}/server_data?instance_id=${INSTANCE_ID} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["op"]')
+read OPS <<< $(curl -k -s ${PHONE_HOME_ENDPOINT}/server_data?instance_id=${INSTANCE_ID} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["op"]')
 echo Ops are $OPS
 echo $OPS > /home/ubuntu/ops.txt
 
