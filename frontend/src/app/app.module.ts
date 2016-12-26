a1f15e73a7844aa289c0bd7838db3f26b79b72e3
Andy <andyfeely@gmail.com>
2016-12-26 18:53:53 +0000

    Added dummy servers json until api CORS

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AuthGuard } from './common/auth.guard';
 import { UserLoginService, CognitoUtil } from "./service/cognito.service";
@@ -22,6 +22,7 @@ import {ServersComponent} from "./servers/servers.component";
     BrowserModule,
     FormsModule,
     HttpModule,
+    JsonpModule,
     AppRoutingModule,
     NavbarModule
   ],
--- frontend/src/app/servers/servers.component.ts
+++ frontend/src/app/servers/servers.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Pipe, PipeTransform} from '@angular/core';
+import {ServersService} from "./servers.service";
+import {Server } from './server';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  providers: [ ServersService ]
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private serversService: ServersService ) { }
+  private servers: Server[];
+  errorMessage: string;
 
   ngOnInit() {
+    this.serversService.getServers()
+      .subscribe(
+        servers => this.servers = servers,
+        error =>  this.errorMessage = <any>error
+      );
   }
-
+  logLog(){
+    console.log(this.servers);
+  }
 }
