276289f70eb4209055f6e1d95b30e94f569ebc6e
Andy <andyfeely@gmail.com>
2016-12-27 17:58:57 +0000

    Changed servers to datatable

--- frontend/src/app/servers/servers.module.ts
+++ frontend/src/app/servers/servers.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServersComponent } from './servers.component';
 import { NavbarModule } from '../navbar/navbar.module';
-import { DataListModule, DialogModule } from 'primeng/primeng';
+import { DataTableModule } from 'primeng/primeng';
 import {ServersRoutingModule} from "./servers-routing.module";
 
 @NgModule({
   imports: [
     CommonModule,
     NavbarModule,
-    DataListModule,
-    DialogModule,
+    DataTableModule,
     ServersRoutingModule
   ],
   declarations: [
