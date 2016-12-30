9b8b2c0875e825970546b847c98712312f19dd9c
Andy <andyfeely@gmail.com>
2016-12-30 19:53:51 +0000

    Changed ark model to have self.connect_port (inside init function) as I was getting errors

--- ark/arkserver.py
+++ ark/arkserver.py
@@ -10,10 +10,9 @@ class ArkServer(Server):
                 'polymorphic_identity':'ark_server',
             }
 
-    connect_port = 7777
-
     def __init__(self, user, size='micro', name='Ark Server'):
         super().__init__(user, size=size, name=name)
+        self.connect_port = 7777
 
     def serialize(self):
         data = super().serialize()
