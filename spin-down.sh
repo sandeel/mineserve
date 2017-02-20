7d0b8d2dbd18d1ecf4665aaffea6e7d927c560cd
Ubuntu <ubuntu@ip-10-0-0-122.eu-west-1.compute.internal>
2017-02-20 10:34:10 +0000

    Empty the CodePipeline bucket to allow the stack to spin down

--- spin-down.sh
+++ spin-down.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo "Getting the name of the CodePipeline S3 Bucket"
+codepipeline_bucket_physical_name=$(aws cloudformation --region eu-west-1 list-stack-resources --stack-name msv --query 'StackResourceSummaries[?LogicalResourceId==`CodePipelineS3Bucket`].[PhysicalResourceId]' --output=text)
+
+if [ -n "$codepipeline_bucket_physical_name" ]; then
+    echo "Emptying the CodePipeline S3 bucket"
+    aws s3 rm s3://$codepipeline_bucket_physical_name --recursive
+else
+    echo "CodePipeline S3 Bucket already deleted"
+fi
+
 aws cloudformation delete-stack \
     --stack-name msv \
     --region eu-west-1
--- spin-up.sh
+++ spin-up.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-aws cloudformation --region eu-west-1 create-stack --stack-name msv --template-body file://cloudformation/cloudformation.yaml --parameters ParameterKey=KeyPair,ParameterValue=id_rsa ParameterKey=FlaskDebug,ParameterValue=True ParameterKey=GithubToken,ParameterValue=$MSV_GITHUB_TOKEN ParameterKey=DatabasePassword,ParameterValue=123moon12 ParameterKey=StripePublishableKey,ParameterValue=$MSV_STRIPE_PK ParameterKey=StripeSecretKey,ParameterValue=$MSV_STRIPE_SK ParameterKey=AdminPassword,ParameterValue=$MSV_ADMIN_PASS ParameterKey=Beta,ParameterValue=True --capabilities CAPABILITY_IAM
+BASE_REGION=eu-west-1
+BASE_STACK_NAME=msv
+
+stack_exists=`aws cloudformation --region $BASE_REGION describe-stacks --stack-name $BASE_STACK_NAME`
+
+if [ -n "$stack_exists" ]; then
+    aws cloudformation --region $BASE_REGION update-stack --stack-name $BASE_STACK_NAME --template-body file://cloudformation/cloudformation.yaml --parameters ParameterKey=KeyPair,ParameterValue=id_rsa ParameterKey=FlaskDebug,ParameterValue=True ParameterKey=GithubToken,ParameterValue=$MSV_GITHUB_TOKEN ParameterKey=DatabasePassword,ParameterValue=123moon12 ParameterKey=StripePublishableKey,ParameterValue=$MSV_STRIPE_PK ParameterKey=StripeSecretKey,ParameterValue=$MSV_STRIPE_SK ParameterKey=AdminPassword,ParameterValue=$MSV_ADMIN_PASS ParameterKey=Beta,ParameterValue=True --capabilities CAPABILITY_IAM
+else
+    aws cloudformation --region $BASE_REGION create-stack --stack-name $BASE_STACK_NAME --template-body file://cloudformation/cloudformation.yaml --parameters ParameterKey=KeyPair,ParameterValue=id_rsa ParameterKey=FlaskDebug,ParameterValue=True ParameterKey=GithubToken,ParameterValue=$MSV_GITHUB_TOKEN ParameterKey=DatabasePassword,ParameterValue=123moon12 ParameterKey=StripePublishableKey,ParameterValue=$MSV_STRIPE_PK ParameterKey=StripeSecretKey,ParameterValue=$MSV_STRIPE_SK ParameterKey=AdminPassword,ParameterValue=$MSV_ADMIN_PASS ParameterKey=Beta,ParameterValue=True --capabilities CAPABILITY_IAM
+fi
