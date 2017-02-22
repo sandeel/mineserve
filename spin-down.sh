9359af59009be52ebf03222911144bcc322eee46
Ubuntu <ubuntu@ip-10-0-0-122.eu-west-1.compute.internal>
2017-02-22 07:08:00 +0000

    Changed to ubuntu 16.04

--- spin-down.sh
+++ spin-down.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+stack_exists=`aws cloudformation --region $BASE_REGION describe-stacks --stack-name $BASE_STACK_NAME`
+stack_status=$(echo $stack_exists | jq '.Stacks[0].StackStatus')
+
+if [ "$stack_status" == "\"CREATE_COMPLETE\"" ]; then
+    echo "Stack already deleted".
+    exit 0
+fi
+
+
 echo "Getting the name of the CodePipeline S3 Bucket"
 codepipeline_bucket_physical_name=$(aws cloudformation --region eu-west-1 list-stack-resources --stack-name msv --query 'StackResourceSummaries[?LogicalResourceId==`CodePipelineS3Bucket`].[PhysicalResourceId]' --output=text)
 
--- spin-up.sh
+++ spin-up.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 BASE_REGION=eu-west-1
-BASE_STACK_NAME=msv2
+BASE_STACK_NAME=msv
+
+: ${MSV_GITHUB_TOKEN?"Need to set MSV_GITHUB_TOKEN. Try running with MSV_GITHUB_TOKEN=xyz123 ./spin-up.sh"}
 
 stack_exists=`aws cloudformation --region $BASE_REGION describe-stacks --stack-name $BASE_STACK_NAME`
 
 if [ -n "$stack_exists" ]; then
+
+    echo "Stack exists. Updating."
+
     aws cloudformation --region $BASE_REGION update-stack --stack-name $BASE_STACK_NAME --template-body file://cloudformation/cloudformation.yaml --parameters ParameterKey=KeyPair,ParameterValue=id_rsa ParameterKey=FlaskDebug,ParameterValue=True ParameterKey=GithubToken,ParameterValue=$MSV_GITHUB_TOKEN ParameterKey=DatabasePassword,ParameterValue=123moon12 ParameterKey=StripePublishableKey,ParameterValue=$MSV_STRIPE_PK ParameterKey=StripeSecretKey,ParameterValue=$MSV_STRIPE_SK ParameterKey=AdminPassword,ParameterValue=$MSV_ADMIN_PASS ParameterKey=Beta,ParameterValue=True --capabilities CAPABILITY_IAM
+
 else
+
+    echo "Launching stack."
+
     aws cloudformation --region $BASE_REGION create-stack --stack-name $BASE_STACK_NAME --template-body file://cloudformation/cloudformation.yaml --parameters ParameterKey=KeyPair,ParameterValue=id_rsa ParameterKey=FlaskDebug,ParameterValue=True ParameterKey=GithubToken,ParameterValue=$MSV_GITHUB_TOKEN ParameterKey=DatabasePassword,ParameterValue=123moon12 ParameterKey=StripePublishableKey,ParameterValue=$MSV_STRIPE_PK ParameterKey=StripeSecretKey,ParameterValue=$MSV_STRIPE_SK ParameterKey=AdminPassword,ParameterValue=$MSV_ADMIN_PASS ParameterKey=Beta,ParameterValue=True --capabilities CAPABILITY_IAM
 fi
+
+exit 0
