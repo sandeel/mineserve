ba8d59921b39804e99dc60d919ab3b331ebc709c
Ubuntu <ubuntu@ip-10-0-0-122.eu-west-1.compute.internal>
2017-03-08 19:43:36 +0000

    Moved up auth function

--- mineserve/views.py
+++ mineserve/views.py
@@ -109,6 +109,64 @@ def server(server_id):
             )
 """
 
+# This is the new auth function
+# I am pretty sure it uses the client secret and id to verify the token
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        client_id = "gJv54phVRi5DcleXXy2ipeCM3J2FmGG5"
+        client_secret = "r-eKjoNIhsf2491X7YYJRYd0_S7uYPIBRlDXfDkZrihJmMeHkOk278UJggwGv91Z"
+        auth = request.headers.get('Authorization', None)
+        if not auth:
+            return handle_error({'code': 'authorization_header_missing',
+                                'description':
+                                    'Authorization header is expected'}, 401)
+
+        parts = auth.split()
+
+        if parts[0].lower() != 'bearer':
+            return handle_error({'code': 'invalid_header',
+                                'description':
+                                    'Authorization header must start with'
+                                    'Bearer'}, 401)
+        elif len(parts) == 1:
+            return handle_error({'code': 'invalid_header',
+                                'description': 'Token not found'}, 401)
+        elif len(parts) > 2:
+            return handle_error({'code': 'invalid_header',
+                                'description': 'Authorization header must be'
+                                 'Bearer + \s + token'}, 401)
+
+        token = parts[1]
+        try:
+            payload = jwt.decode(
+                token,
+                client_secret,
+                audience=client_id
+            )
+        except jwt.ExpiredSignature:
+            return handle_error({'code': 'token_expired',
+                                'description': 'token is expired'}, 401)
+        except jwt.InvalidAudienceError:
+            return handle_error({'code': 'invalid_audience',
+                                'description': 'incorrect audience, expected: '
+                                 + client_id}, 401)
+        except jwt.DecodeError:
+            return handle_error({'code': 'token_invalid_signature',
+                                'description':
+                                    'token signature is invalid'}, 401)
+        except Exception:
+            return handle_error({'code': 'invalid_header',
+                                'description': 'Unable to parse authentication'
+                                 ' token.'}, 400)
+
+        _app_ctx_stack.top.current_user = payload
+        return f(*args, **kwargs)
+
+    return decorated
+
+
 
 # set up jwt
 def intarr2long(arr):
@@ -231,64 +289,6 @@ def handle_error(error, status_code):
     resp = jsonify(error)
     resp.status_code = status_code
     return resp
-
-
-# This is the new auth function
-# I am pretty sure it uses the client secret and id to verify the token
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        client_id = "gJv54phVRi5DcleXXy2ipeCM3J2FmGG5"
-        client_secret = "r-eKjoNIhsf2491X7YYJRYd0_S7uYPIBRlDXfDkZrihJmMeHkOk278UJggwGv91Z"
-        auth = request.headers.get('Authorization', None)
-        if not auth:
-            return handle_error({'code': 'authorization_header_missing',
-                                'description':
-                                    'Authorization header is expected'}, 401)
-
-        parts = auth.split()
-
-        if parts[0].lower() != 'bearer':
-            return handle_error({'code': 'invalid_header',
-                                'description':
-                                    'Authorization header must start with'
-                                    'Bearer'}, 401)
-        elif len(parts) == 1:
-            return handle_error({'code': 'invalid_header',
-                                'description': 'Token not found'}, 401)
-        elif len(parts) > 2:
-            return handle_error({'code': 'invalid_header',
-                                'description': 'Authorization header must be'
-                                 'Bearer + \s + token'}, 401)
-
-        token = parts[1]
-        try:
-            payload = jwt.decode(
-                token,
-                client_secret,
-                audience=client_id
-            )
-        except jwt.ExpiredSignature:
-            return handle_error({'code': 'token_expired',
-                                'description': 'token is expired'}, 401)
-        except jwt.InvalidAudienceError:
-            return handle_error({'code': 'invalid_audience',
-                                'description': 'incorrect audience, expected: '
-                                 + client_id}, 401)
-        except jwt.DecodeError:
-            return handle_error({'code': 'token_invalid_signature',
-                                'description':
-                                    'token signature is invalid'}, 401)
-        except Exception:
-            return handle_error({'code': 'invalid_header',
-                                'description': 'Unable to parse authentication'
-                                 ' token.'}, 400)
-
-        _app_ctx_stack.top.current_user = payload
-        return f(*args, **kwargs)
-
-    return decorated
 
 
 @application.route("/secured/ping", methods=["GET"])
