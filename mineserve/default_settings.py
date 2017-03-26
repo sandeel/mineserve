8215b0a81df2fa2d7ec2708bb06eea1eb58364df
Ubuntu <ubuntu@ip-10-0-0-122.eu-west-1.compute.internal>
2017-03-26 10:42:32 +0000

    add additional regions

--- mineserve/default_settings.py
+++ mineserve/default_settings.py
@@ -7,9 +7,21 @@ APP_NAME = os.getenv('APP_NAME',default='msv-testing')
 AWS_REGION = "eu-west-1"
 EC2_KEYPAIR = "id_rsa"
 CONTAINER_AGENT_INSTANCE_PROFILE = "mineserve-agent"
+
+
+
+
+# AMIs for container agents
 CONTAINER_AGENT_AMI = {}
-CONTAINER_AGENT_AMI['eu-west-1'] = "ami-95f8d2f3"
-CONTAINER_AGENT_AMI['us-east-1'] = "ami-b2df2ca4"
+CONTAINER_AGENT_AMI['us-east-1'] = "ami-b2df2ca4" # n. virginia
+CONTAINER_AGENT_AMI['us-east-2'] = "ami-62745007" # ohio
+CONTAINER_AGENT_AMI['eu-west-1'] = "ami-95f8d2f3" # ireland
+CONTAINER_AGENT_AMI['us-west-2'] = "ami-62d35c02" # oregon
+CONTAINER_AGENT_AMI['ap-southeast-2'] = "ami-fbe9eb98" # sydney
+
+
+
+
 STUB_AWS_RESOURCES = (os.getenv('STUB_AWS_RESOURCES', 'True') == 'True')
 
 # app settings
